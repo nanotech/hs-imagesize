b13c6d6f94442cbcae8869b3ca9b5cac89782c6c
NanoTech <nanotech@nanotechcorp.net>
2014-04-28 02:31:52 -0600

    Everything is a parser, and other refactorings

--- Graphics/ImageSize.hs
+++ Graphics/ImageSize.hs
@@ -1,11 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings
+           , TupleSections
+           #-}
 module Graphics.ImageSize
-( Size (..)
+(
+  -- * Parsers
+  imageFormatParser
+, imageSizeParser
+, imageInfoParser
+  -- * Types
+, Size (..)
 , FileFormat (..)
-, detectFileFormat
-, parseImageSize
-, imageSize
 ) where
+
+-- Magic number references:
+--
+-- PNG: http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html (3.1 PNG file signature)
+-- GIF: http://www.w3.org/Graphics/GIF/spec-gif89a.txt (17. Header)
+--
+-- JPEG parsing references:
+--
+-- http://www.faqs.org/faqs/jpeg-faq/part1/
+-- https://gears.googlecode.com/svn/trunk/third_party/libjpeg/rdjpgcom.c
+-- http://kd5col.info/swag/GRAPHICS/0143.PAS.html
+-- http://hackage.haskell.org/package/imagesize-conduit-1.0.0.2/docs/src/Data-Conduit-ImageSize.html
 
 import Data.Maybe
 import Data.List (find)
@@ -19,64 +36,55 @@ import Data.Word (Word8)
 import System.IO (openBinaryFile, IOMode (ReadMode))
 
 import qualified Data.Attoparsec.ByteString as P
+import qualified Data.Attoparsec.Combinator as P
 import Data.Attoparsec.ByteString (Parser)
 
-data Size = Size { width :: Int, height :: Int }
-  deriving (Show, Eq, Ord, Read)
+-- | The pixel dimensions of an image.
+data Size = Size { width :: !Int, height :: !Int }
+  deriving (Show, Read, Eq, Ord)
 
+-- | The file format of an image.
 data FileFormat = GIF | PNG | JPEG
-  deriving (Show, Eq, Ord, Read, Enum)
+  deriving (Show, Read, Eq, Ord, Enum)
 
-justIf :: a -> Bool -> Maybe a
-justIf x b = if b then Just x else Nothing
+allFileFormats :: [FileFormat]
+allFileFormats = enumFrom GIF
 
-findJust :: [Maybe a] -> Maybe a
-findJust = join . find isJust
-
-hush :: Either a b -> Maybe b
-hush = either (const Nothing) Just
-
-jpgSignature, gifSignature, pngSignature :: ByteString
-jpgSignature = "\xff\xd8"
-gifSignature = "GIF8"
-pngSignature = "\x89PNG\r\n\x1a\n" 
-
-fileFormatMatchers :: [ByteString -> Maybe FileFormat]
-fileFormatMatchers =
-  [ justIf JPEG . B.isPrefixOf jpgSignature
-  , justIf GIF . B.isPrefixOf gifSignature
-  , justIf PNG . B.isPrefixOf pngSignature
-  ]
-
--- | Try to detect the format of an image by its magic number (header bytes).
-detectFileFormat :: ByteString -> Maybe FileFormat
-detectFileFormat s = findJust $ map ($ s) fileFormatMatchers
-
--- | Parse a pair of integers of a certain width in bytes.
-parseSize :: (ByteString -> Int) -> Int -> Parser Size
-parseSize readInt n = size <$> P.take n <*> P.take n
-  where size w h = Size (readInt w) (readInt h)
-
--- | A parser that reads the 'Size' of an image with a certain 'FileFormat'.
-parseImageSize :: FileFormat -> Maybe (Parser Size)
-parseImageSize ff = case ff of
-  PNG -> Just $ P.take 16 *> parseSize readBEInt 4
-  GIF -> Just $ P.take 6 *> parseSize readLEInt 2
-  JPEG -> Just $ parseJPEGSize
-  _ -> Nothing
+formatSignature :: FileFormat -> ByteString
+formatSignature ff = case ff of
+  PNG  -> "\x89PNG\r\n\x1a\n"
+  JPEG -> "\xff\xd8"
+  GIF  -> "GIF"
 
 
--- JPEG parsing references:
---
--- http://www.faqs.org/faqs/jpeg-faq/part1/
--- https://gears.googlecode.com/svn/trunk/third_party/libjpeg/rdjpgcom.c
--- http://kd5col.info/swag/GRAPHICS/0143.PAS.html
--- http://hackage.haskell.org/package/imagesize-conduit-1.0.0.2/docs/src/Data-Conduit-ImageSize.html
+-- | Tries to detect the format of an image by its magic number (header bytes).
+imageFormatParser :: Parser FileFormat
+imageFormatParser = P.choice $ map
+  (\ff -> P.string (formatSignature ff) *> pure ff)
+  allFileFormats
 
-parseJPEGSize :: Parser Size
-parseJPEGSize = do
-  _ <- P.string jpgSignature -- SOI
-  parseJPEGSizeSegment
+-- | Tries to parse the 'Size' of an image. The 'FileFormat' is determined
+-- automatically with 'imageFormatParser'.
+imageSizeParser :: Parser Size
+imageSizeParser = fst <$> imageInfoParser
+
+-- | Combines the output of 'imageSizeParser' and 'imageFormatParser'.
+imageInfoParser :: Parser (Size, FileFormat)
+imageInfoParser = do
+  ff <- imageFormatParser
+  (,ff) <$> case ff of
+    PNG ->
+      P.take 8 -- skip IHDR chunk length and type
+      *> sizeParser readBEInt 4
+
+    GIF -> do
+      P.take 3 -- skip version bytes; 87a or 89a
+      *> sizeParser readLEInt 2
+
+    JPEG -> parseJPEGSizeSegment
+
+
+-- JPEG parsing
 
 parseJPEGSegment :: Parser (Word8, Int)
 parseJPEGSegment = do
@@ -91,16 +99,17 @@ parseJPEGSizeSegment = do
   if segmentType >= 0xC0 && segmentType <= 0xCF -- SOFn
     then do
       _ <- P.anyWord8 -- skip the data precision field
-      Size h w <- parseSize readBEInt 2
+      Size h w <- sizeParser readBEInt 2
       return $ Size w h -- JPEG writes the size backwards compared to other formats
     else do
       _ <- P.take segmentLen
       parseJPEGSizeSegment
 
 
--- | Read the 'Size' of an image with a certain 'FileFormat'.
-imageSize :: FileFormat -> ByteString -> Maybe Size
-imageSize ff s = parseImageSize ff >>= \parser -> hush (P.parseOnly parser s)
+-- | Parse a pair of integers of a certain width in bytes.
+sizeParser :: (ByteString -> Int) -> Int -> Parser Size
+sizeParser readInt n = size <$> P.take n <*> P.take n
+  where size w h = Size (readInt w) (readInt h)
 
 -- | Interpret a 'ByteString' as an unsigned big-endian integer.
 readBEInt :: ByteString -> Int
@@ -113,14 +122,14 @@ readLEInt = readSomeInt $ B.foldl . flip
 readSomeInt :: Integral byte => ((byte -> (Int, Int) -> (Int, Int)) -> (Int, Int) -> a -> (c, b)) -> a -> c
 readSomeInt fold = fst . fold (\c (a,i) -> (a + (fromIntegral c `shiftL` i), i + 8)) (0,0)
 
+
 test :: String -> IO ()
 test path = do
   h <- openBinaryFile path ReadMode
   print h
   s <- B.hGet h 200
-  let mff = detectFileFormat s
-  print mff
-  print $ mff >>= (`imageSize` s)
+  print $ P.parseOnly imageFormatParser s
+  print $ P.parseOnly imageSizeParser s
   putStrLn ""
 
 main :: IO ()
